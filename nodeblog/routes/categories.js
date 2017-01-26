aa7b899d5b38458b54f694c5fa3a3914b703b0bc
Knox316 <fernandocorreia316@gmail.com>
2017-01-26 20:12:06 +0000

    Simple NodeJS Blog
    Simple NodeJS Blog using Express to inser comments and images

---
+++ nodeblog/routes/categories.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+
+router.get('/show/:category', function(req,res,next){
+    var db = req.db;
+    var posts = db.get('posts');
+    posts.find({category: req.params.category},{},function(err, posts){
+        res.render('index',{
+            "title":"Add Category",
+            "posts": posts
+        });
+    });
+});
+
+
+/*category homepage */
+router.get('/add', function(req, res, next) {
+    res.render('addcategory', {
+        "title":"Add category"
+    });
+});
+
+router.post('/add', function(req, res, next){
+    //get form value    
+    var title = req.body.title;
+
+    //form validation
+    req.checkBody('title', 'Title field is required').notEmpty();
+
+    //check erros
+    var errors = req.validationErrors();
+    if(errors){
+        res.render('addcategory',{
+            "errors": errors,
+            "title": title,
+        });
+    }else{
+        var categories = db.get('categories');
+
+        //submit to db
+        categories.insert({
+            "title":title
+        }, function(err, category){
+            if(err){
+                res.send('There was an issue submitting the post');
+            }else{
+                req.flash('success', 'Category submitted');
+                res.location('/');
+                res.redirect('/');
+            }
+        });
+    }
+});
+
+module.exports = router;
